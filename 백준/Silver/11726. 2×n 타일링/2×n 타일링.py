551d7fa7c752c33b0ca0b840da3b57684db7aa1f
GunWoo Park <rjsdn7895@gnu.ac.kr>
2025-12-29 08:46:16 +0900

    [Silver III] Title: 2×n 타일링, Time: 92 ms, Memory: 108384 KB -BaekjoonHub

--- 백준/Silver/11726. 2×n 타일링/2×n 타일링.py
+++ 백준/Silver/11726. 2×n 타일링/2×n 타일링.py
@@ -3,25 +3,20 @@ import sys
 def input():
     return sys.stdin.readline().rstrip()
 
-
-
 n = int(input())
 
 dp = [-1] * (n+1)
 
+
 dp[1] = 1
-if n  > 1 :
-    dp[2] = 2
+if n>= 2 : dp[2] = 2 
 
-if n >= 3 :
-
-    for i in range(3, n+1) :
+for i in range(3,n+1) :
     
-        dp[i] = dp[i-1] + dp[i-2]
+
+    dp[i] = dp[i-1] + dp[i-2]
 
 
+res = dp[n] % 10007
 
-res = dp[n]
-
-print(res % 10007)
-
+print(res)
