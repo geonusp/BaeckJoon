c0c6e69e6b746b4661c0bb940ecad58f3a390548
Gunwoo Park <rjsdn7895@gnu.ac.kr>
2026-01-06 07:33:51 +0900

    [Gold IV] Title: 타일 채우기, Time: 88 ms, Memory: 108384 KB -BaekjoonHub

--- 백준/Gold/2133. 타일 채우기/타일 채우기.py
+++ 백준/Gold/2133. 타일 채우기/타일 채우기.py
@@ -5,25 +5,22 @@ def input():
 
 N = int(input())
 
-dp = [-1] * (N+1)
+dp = [0] * (30+1)
 
-dp[0] = 1 
-dp[1] = 0
-if N > 1 : dp[2] = 3
+dp[0] = 1
+dp[1] = 0 
+dp[2] = 3
 
-if N >= 3 :
+
+for i in range(3, N+1) :
     
-    for i in range(3,N+1) :
+    if i % 2 == 0 :
+        dp[i] = 4*dp[i-2] - dp[i-4]
 
-        if i % 2 == 0 :
-            result = 0
-            result += 3 * dp[i-2]
-            for j in range(2,(i//2)+1,1):
-                result += 2*dp[i-2*(j)]
 
-            dp[i] = result
 
-        elif i % 2 != 0 :
-            dp[i] = 0
+    else : 
+        dp[i] = 0
 
-print(dp[N]) 
+
+print(dp[N])
