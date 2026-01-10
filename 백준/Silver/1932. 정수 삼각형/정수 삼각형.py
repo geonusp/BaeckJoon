06d18d511465ac3fcfd393637e1858e852bd37b7
Gunwoo Park <rjsdn7895@gnu.ac.kr>
2026-01-10 12:19:20 +0900

    [Silver I] Title: 정수 삼각형, Time: 108 ms, Memory: 113604 KB -BaekjoonHub

--- 백준/Silver/1932. 정수 삼각형/정수 삼각형.py
+++ 백준/Silver/1932. 정수 삼각형/정수 삼각형.py
@@ -3,33 +3,26 @@ import sys
 def input():
     return sys.stdin.readline().rstrip()
 
+
 n = int(input())
 
 py = {}
 
+dp = [[0] * (n+1) for _ in range(n+1)]
+
 for index in range(1,n+1) :
-    line = list(map(int,input().split()))
+    line = list(map(int, input().split()))
     line.insert(0,0)
     py[index] = line
 
 
-dp = [[0] *(n+1) for _ in range(n+1)]
-
 
 for i in range(1,n+1) :
 
-    for j in range(1,i+1) : # 1~i
-
-        dp[i][j] = max(dp[i-1][j-1], dp[i-1][j]) + py[i][j] 
-
-
-max = dp[n][1]
-
-for col in range(1,n) :
-
-    if dp[n][col+1] > max :
-
-        max = dp[n][col+1]
+    for j in range(1,i+1) :
+        dp[i][j] = max(dp[i-1][j-1], dp[i-1][j]) + py[i][j]
 
 
-print(max)
+res = max(dp[n])
+
+print(res)
