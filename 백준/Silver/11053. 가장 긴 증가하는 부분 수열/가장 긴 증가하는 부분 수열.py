ddf162154580a65fbbe6e4a657abba1ee37fdcab
Gunwoo Park <rjsdn7895@gnu.ac.kr>
2026-01-11 01:22:58 +0900

    [Silver II] Title: 가장 긴 증가하는 부분 수열, Time: 100 ms, Memory: 109544 KB -BaekjoonHub

---
+++ 백준/Silver/11053. 가장 긴 증가하는 부분 수열/가장 긴 증가하는 부분 수열.py
@@ -0,0 +1,20 @@
+import sys 
+
+def input():
+    return sys.stdin.readline().rstrip()
+
+N = int(input())
+
+A = list(map(int,input().split()))
+
+dp = [1] * (N)
+
+for i in range(0,N) :
+
+    for j in range(0,i) :
+        if A[i] > A[j] :
+            
+            dp[i] = max(dp[i], dp[j] + 1)
+
+
+print(max(dp))
