5037ff910c42e73e0f9811c9a90b9c8f326c070e
Gunwoo Park <rjsdn7895@gnu.ac.kr>
2026-01-15 10:49:33 +0900

    [Gold V] Title: 회의실 배정, Time: 348 ms, Memory: 131248 KB -BaekjoonHub

---
+++ 백준/Gold/1931. 회의실 배정/회의실 배정.py
@@ -0,0 +1,32 @@
+import sys 
+
+def input():
+    return sys.stdin.readline().rstrip()
+
+N = int(input())
+
+table = []
+
+for _ in range(N) :
+
+    line = list(map(int,input().split()))
+    table.append(line)
+
+sorted_table = sorted(table, key=lambda x: (x[1],x[0]))
+
+
+
+temp = sorted_table[0][1]
+
+count = 1
+
+for i in range(0,N-1) :
+
+    if sorted_table[i+1][0] >= temp :
+
+        temp = sorted_table[i+1][1]
+        count += 1
+
+print(count)
+
+
