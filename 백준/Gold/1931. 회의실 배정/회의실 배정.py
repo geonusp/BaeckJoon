b6cf04d70836618730f4c5777d16c369493de43e
Gunwoo Park <rjsdn7895@gnu.ac.kr>
2026-01-15 11:04:15 +0900

    [Gold V] Title: 회의실 배정, Time: 332 ms, Memory: 131248 KB -BaekjoonHub

--- 백준/Gold/1931. 회의실 배정/회의실 배정.py
+++ 백준/Gold/1931. 회의실 배정/회의실 배정.py
@@ -1,4 +1,4 @@
-import sys 
+import sys
 
 def input():
     return sys.stdin.readline().rstrip()
@@ -9,24 +9,22 @@ table = []
 
 for _ in range(N) :
 
-    line = list(map(int,input().split()))
-    table.append(line)
+    item = list(map(int,input().split()))
+    table.append(item)
 
-sorted_table = sorted(table, key=lambda x: (x[1],x[0]))
-
+sorted_table = sorted(table, key = lambda x: (x[1],x[0]))
 
 
 temp = sorted_table[0][1]
 
 count = 1
 
-for i in range(0,N-1) :
+for i in range(0,N-1) : 
 
     if sorted_table[i+1][0] >= temp :
 
         temp = sorted_table[i+1][1]
         count += 1
 
+
 print(count)
-
-
