cb1f94bbaa201d47833613f9b201e4c7076e09aa
Gunwoo Park <rjsdn7895@gnu.ac.kr>
2026-02-06 02:30:35 +0900

    [Bronze I] Title: 캠핑, Time: 76 ms, Memory: 108384 KB -BaekjoonHub

--- 백준/Bronze/4796. 캠핑/캠핑.py
+++ 백준/Bronze/4796. 캠핑/캠핑.py
@@ -3,31 +3,28 @@ import sys
 def input():
     return sys.stdin.readline().rstrip()
 
-L = 100
-P = 100
-V = 100
+L = -1
+P = -1
+V = -1
 
 
-i = 1
+i = 0
 
 while True :
+    res = 0
+    i += 1
 
     L, P, V = map(int, input().split())
 
-    if L == 0 and P == 0 and V == 0 :
-        break
+    if L != 0 and P != 0 and V != 0 :
+        
+        if V % P > L :
+            res += L * (V//P + 1)
 
-    count = V // P
+        else :
+            res += L*(V//P) + (V%P)
 
-    if V - (P * count) > L :
-        res = (count+1) * (L)
-    else :
-        res = V - (count * (P-L))
-
-    print(f"Case {i}: {res}")
-
-    i += 1
-
-    if L == 0 and P == 0 and V == 0 :
-        break
+        print(f"Case {i}: {res}")
     
+    else : 
+        break
