30b8101d4a239191efecb3354622a70fb70c7328
Gunwoo Park <rjsdn7895@gnu.ac.kr>
2026-02-20 19:22:04 +0900

    [Silver V] Title: 수들의 합, Time: 88 ms, Memory: 108384 KB -BaekjoonHub

--- 백준/Silver/1789. 수들의 합/수들의 합.py
+++ 백준/Silver/1789. 수들의 합/수들의 합.py
@@ -5,30 +5,28 @@ def input():
 
 S = int(input())
 
-start = 1 # 자연수의 개수 
+start = 1
 end = S
 res = 0
 
 while start <= end :
+    
     mid = (start+end) // 2
-    sum_mid = (mid*(mid+1)) // 2   # 1부터 mid 까지 합 
 
-    if sum_mid > S :
-        end = mid - 1
+    sum_mid = (mid*(mid+1)) // 2
+
+    if sum_mid < S :
+        res = mid
+        start = mid + 1
+        
 
     elif sum_mid == S :
         res = mid 
         break
 
-    elif sum_mid < S :
-        start = mid + 1
-        res = mid 
-        
+    elif sum_mid > S :
+        end = mid - 1
 
 
 print(res)
-
-
-
-
 
