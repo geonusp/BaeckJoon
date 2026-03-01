6f61029d21ca4ca7503d4174c831c8ea21425867
Gunwoo Park <rjsdn7895@gnu.ac.kr>
2026-03-01 17:17:21 +0900

    [Gold V] Title: 동전 2, Time: 104 ms, Memory: 110780 KB -BaekjoonHub

--- 백준/Gold/2294. 동전 2/동전 2.py
+++ 백준/Gold/2294. 동전 2/동전 2.py
@@ -5,32 +5,39 @@ def input():
 
 positive_inf = float('inf')
 
+
 n, k = map(int, input().split())
 
-pocket = []
+coin_list = []
 
 for _ in range(n) :
     line = int(input())
-    pocket.append(line)
-
-memo = [positive_inf] * (k+1)
-
-memo[0] = 0
-
-for i in range(1,k+1) :
-
-    for j in range(len(pocket)) :
-
-        prev = i - pocket[j]
-
-        if prev >= 0 :
-            temp = memo[prev] + 1
-
-            if temp < memo[i] :
-                memo[i] = temp
+    coin_list.append(line)
 
 
-if memo[k] == positive_inf :
+dp = [positive_inf] * (k+1)
+
+dp[0] = 0
+
+for won in range (0,k+1) :
+
+    if dp[won] != positive_inf :
+        continue
+
+
+    for coin in (coin_list) :
+
+        if (won - coin) >= 0 :
+            temp = dp[won-coin] + 1
+            
+            if temp < dp[won] :
+                dp[won] = temp
+
+
+res = dp[k]
+
+if res == positive_inf :
     print(-1)
+
 else :
-    print(memo[k])
+    print(res)
