92ba3db7209e176e1ce24eda9a8c44d187ef24ed
Gunwoo Park <rjsdn7895@gnu.ac.kr>
2026-03-31 18:07:00 +0900

    [Silver III] Title: 주유소, Time: 124 ms, Memory: 134160 KB, Score: 17 point -BaekjoonHub

--- 백준/Silver/13305. 주유소/주유소.py
+++ 백준/Silver/13305. 주유소/주유소.py
@@ -3,19 +3,32 @@ import sys
 def input():
     return sys.stdin.readline().rstrip()
 
-N = int(input())
-
-
+n = int(input())
 dist = list(map(int, input().split()))
-weight = list(map(int, input().split()))
+price = list(map(int, input().split()))
 
-apply = []
+answer = 0
 
-res = 0
+temp = set()   # 최신 최솟값찾아내는 역할(중복제거)
 
-for i in range(0,N-1) :
-    apply.append(weight[i])
-    res += dist[i] * min(apply)
+for i in range(len(dist)) :
+    # 처음이라 특수 
+    if i == 0 :
+        answer += dist[0] * price[0]
+        continue
+
+    # 마지막도 특수 
+    if i != len(dist) - 1 :
+        temp.add(price[i])
+        temp.add(price[i+1])
+
+    m = min(temp)
+
+    answer += dist[i] * m
+
+print(answer)
+        
+    
 
 
-print(res)
+
