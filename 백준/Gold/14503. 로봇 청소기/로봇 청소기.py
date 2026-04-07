59aa589f99c6b29ae6e1c062514fc199930b14cf
Gunwoo Park <rjsdn7895@gnu.ac.kr>
2026-04-08 01:45:41 +0900

    [Gold V] Title: 로봇 청소기, Time: 92 ms, Memory: 109544 KB -BaekjoonHub

--- 백준/Gold/14503. 로봇 청소기/로봇 청소기.py
+++ 백준/Gold/14503. 로봇 청소기/로봇 청소기.py
@@ -1,95 +1,13 @@
 import sys
 
-from collections import deque
-
 def input():
     return sys.stdin.readline().rstrip()
-
-def sear(start, N, M):
-
-    r,c,d = start 
-
-    visited = [(r,c)]
-
-    dr = [-1, 0, 1, 0]
-    dc = [0, 1, 0, -1]
-
-
-    while True :
-
-        moved = False
-            
-
-        for i in range(4) :
-
-            # 반시계 회전
-            nd = (d + 3) % 4
-
-            nr = r + dr[nd]
-            nc = c + dc[nd]
-
-            d = nd
-
-
-            #  범위 체크 및 벽 여부
-            if 0 <= nr < N and 0 <= nc < M and table[nr][nc] != 1 and (nr,nc) not in visited :
-                visited.append((nr,nc))
-                r = nr
-                c = nc
-                d = nd
-                moved = True
-                break
-                
-        if moved == True :
-            continue
-        # 주변 4칸 중 청소 안된 빈칸없으면
-        
-        # 후진 가능하면 후진
-        # d 0
-        if d == 0 :
-            rr = r + 1
-            rc = c
-            if 0 <= rr < N and 0 <= rc < M and table[rr][rc] != 1 :
-                r = rr
-                c = rc
-            else : 
-                return len(visited)
-        # d 1
-        elif d == 1 :
-            rr = r
-            rc = c - 1
-            if 0 <= rr < N and 0 <= rc < M and table[rr][rc] != 1 :
-                r = rr
-                c = rc
-            else : 
-                return len(visited)
-        # d 2
-        elif d == 2 :
-            rr = r - 1
-            rc = c
-            if 0 <= rr < N and 0 <= rc < M and table[rr][rc] != 1 :
-                r = rr
-                c = rc
-            else : 
-                return len(visited)
-        # d 3
-        elif d == 3 :
-            rr = r
-            rc = c + 1
-            if 0 <= rr < N and 0 <= rc < M and table[rr][rc] != 1 :
-                r = rr
-                c = rc
-            else : 
-                return len(visited)
-    
-    
-    
 
 # 행 열 
 N, M = map(int, input().split())
 
 # 시작 좌표(r,c), 방향 (0:북, 1:동, 2:남, 3:서)
-start_r, start_c, start_d = map(int, input().split())
+r, c, d = map(int, input().split())
 
 table = []
 
@@ -97,6 +15,77 @@ for _ in range(N) :
     line = list(map(int, input().split()))
     table.append(line)
 
-ans = sear((start_r,start_c,start_d), N, M)
+# 최대 2500 이라 연산 널널하므로 완탐 가능 
 
-print(ans)
+dr = [-1,0,1,0]
+dc = [0,1,0,-1]
+
+# 처음 위치 청소 
+visited = [(r,c)]
+
+while True :
+
+    moved = False
+
+    # 4방향 
+    for _ in range(4) :
+
+        nd = (d+3) % 4
+
+        nr = r + dr[nd]
+
+        nc = c + dc[nd]
+
+        # 방향 갱신 
+        d = nd
+
+        # 범위 확인 및 벽인지 확인 
+        if 0 <= nr < N and 0 <= nc < M and table[nr][nc] != 1 and (nr,nc) not in visited :
+            visited.append((nr,nc))
+            moved = True # 중첩 반복문으로 인한 플래그 
+            r = nr
+            c = nc
+            break
+    
+    if moved == True :
+        continue
+
+    # 주변 4칸중 모두 청소할 곳 없을때 
+    # 4방향 모두 방향갱신되면 원래방향이라 d 갱신 할 필요없음 
+    if d == 0 :
+        rr = r + 1
+        rc = c
+        if 0 <= rr < N and 0 <= rc < M and table[rr][rc] != 1 :
+            r = rr
+            c = rc
+        else :
+            break
+
+    elif d == 1 :
+        rr = r
+        rc = c - 1
+        if 0 <= rr < N and 0 <= rc < M and table[rr][rc] != 1 :
+            r = rr
+            c = rc
+        else :
+            break
+
+    elif d == 2 :
+        rr = r - 1
+        rc = c
+        if 0 <= rr < N and 0 <= rc < M and table[rr][rc] != 1 :
+            r = rr
+            c = rc
+        else :
+            break
+
+    elif d == 3 :
+        rr = r 
+        rc = c + 1
+        if 0 <= rr < N and 0 <= rc < M and table[rr][rc] != 1 :
+            r = rr
+            c = rc
+        else :
+            break
+
+print(len(visited))
