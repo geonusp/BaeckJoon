63a1315b206d91a15e6aa8239d7fb0438467b6c7
Gunwoo Park <rjsdn7895@gnu.ac.kr>
2026-04-07 23:22:30 +0900

    [Gold V] Title: 로봇 청소기, Time: 116 ms, Memory: 113500 KB -BaekjoonHub

--- 백준/Gold/14503. 로봇 청소기/로봇 청소기.py
+++ 백준/Gold/14503. 로봇 청소기/로봇 청소기.py
@@ -1,84 +1,102 @@
 import sys
-sys.setrecursionlimit(10**6)
+
+from collections import deque
 
 def input():
     return sys.stdin.readline().rstrip()
 
+def sear(start, N, M):
 
-"""
-4칸 탐색시도 -> 후진시도 -> 
-"""
+    r,c,d = start 
 
+    visited = [(r,c)]
 
-def dfs(curr_coor, direction, visited):
-    # dfs드갈때 마다 +1
-    r, c = curr_coor
-
-    # 1. 현재 칸이 아직 청소되지 않은 경우, 현재 칸을 청소한다.
-    # 경계점인지 아닌지, 벽 유무, 방문여부 
-        # 청소와 방문처리
-    visited.add(curr_coor)
+    dr = [-1, 0, 1, 0]
+    dc = [0, 1, 0, -1]
 
 
-    # 2. 바라보는 방향을 기준으로 앞쪽 칸이 청소되지 않은 빈 칸인 경우 한 칸 전진한다.
-    # direction 보존 해야하기 때문에 임시 변수 선언 
-    temp_direction = direction
-    for _ in range(4):
+    while True :
+
+        moved = False
+            
+
+        for i in range(4) :
+
+            # 반시계 회전
+            nd = (d + 3) % 4
+
+            nr = r + dr[nd]
+            nc = c + dc[nd]
+
+            d = nd
+
+
+            #  범위 체크 및 벽 여부
+            if 0 <= nr < N and 0 <= nc < M and table[nr][nc] != 1 and (nr,nc) not in visited :
+                visited.append((nr,nc))
+                r = nr
+                c = nc
+                d = nd
+                moved = True
+                break
+                
+        if moved == True :
+            continue
+        # 주변 4칸 중 청소 안된 빈칸없으면
         
-        # 1. 반시계 방향으로 90도 회전한다.(나머지연산(모듈러연산) 이용해 반시계 90도 회전 구현)
-        # 회전이라 원래 방향 변경 
-        temp_direction = (temp_direction+3) % 4
-        next_r = r + dr[temp_direction]
-        next_c = c + dc[temp_direction]
-
-        # 2. 현재 칸의 주변 4칸 중 청소되지 않은 빈 칸이 있는 경우("청소되지않은"이라는 조건이 있으므로 visited 여부 확인)
-        if 0 <= next_r < N and 0 <= next_c < M and board[next_r][next_c] != 1 and (next_r,next_c) not in visited:
-        
-            dfs((next_r, next_c), temp_direction, visited)
-
-            # 후진 로직 실행하지 않도록 즉시 return
-            # 할일 마친거      
-            return 
-        
-    # 후진 로직 
-    # 원래 방향(direction)을 기준으로 뒤쪽 계산
-    back_r = r - dr[direction]
-    back_c = c - dc[direction]
-
-    # 청소기는 이미 청소한 칸에 후진가능  
-    if 0 <= back_r < N and 0 <= back_c < M and board[back_r][back_c] != 1:
-        # 후진시에는 원래 방향 유지!        
-        dfs((back_r, back_c), direction, visited)
-    # 다해봤는데 안된다는거 
-    return 
-
+        # 후진 가능하면 후진
+        # d 0
+        if d == 0 :
+            rr = r + 1
+            rc = c
+            if 0 <= rr < N and 0 <= rc < M and table[rr][rc] != 1 :
+                r = rr
+                c = rc
+            else : 
+                return len(visited)
+        # d 1
+        elif d == 1 :
+            rr = r
+            rc = c - 1
+            if 0 <= rr < N and 0 <= rc < M and table[rr][rc] != 1 :
+                r = rr
+                c = rc
+            else : 
+                return len(visited)
+        # d 2
+        elif d == 2 :
+            rr = r - 1
+            rc = c
+            if 0 <= rr < N and 0 <= rc < M and table[rr][rc] != 1 :
+                r = rr
+                c = rc
+            else : 
+                return len(visited)
+        # d 3
+        elif d == 3 :
+            rr = r
+            rc = c + 1
+            if 0 <= rr < N and 0 <= rc < M and table[rr][rc] != 1 :
+                r = rr
+                c = rc
+            else : 
+                return len(visited)
+    
+    
     
 
+# 행 열 
+N, M = map(int, input().split())
 
-board = []
+# 시작 좌표(r,c), 방향 (0:북, 1:동, 2:남, 3:서)
+start_r, start_c, start_d = map(int, input().split())
 
-dr = [-1,0,1,0]
-dc = [0,1,0,-1]
+table = []
 
-visited = set()
+for _ in range(N) :
+    line = list(map(int, input().split()))
+    table.append(line)
 
-N, M = map(int,input().split())
+ans = sear((start_r,start_c,start_d), N, M)
 
-init_r, init_c, direction = map(int,input().split())
-
-init_coor = (init_r, init_c)
-
-
-for _ in range(N):
-
-    line = list(map(int,input().split()))
-    board.append(line)
-
-dfs(init_coor, direction, visited)
-
-print(len(visited))
-
-
-
-
-
+print(ans)
